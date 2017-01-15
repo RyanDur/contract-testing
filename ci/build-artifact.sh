9e23a321670011eeebb426fa99c57df5350b98cd
RyanDur <ryan.p.durling@gmail.com>
2017-01-14 19:52:02 -0600

    rsync

--- ci/build-artifact.sh
+++ ci/build-artifact.sh
@@ -6,6 +6,10 @@ export TERM=${TERM:-dumb}
 
 apt-get update
 
+apt-get -y install sudo
+
+useradd -m docker && echo "docker:docker" | chpasswd && adduser docker sudo
+
 apt-get -y install openssh-server
 
 apt-get -y install ufw

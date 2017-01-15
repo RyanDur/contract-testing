6b695206aa888b10da7c4027692c727ce5bd873b
RyanDur <ryan.p.durling@gmail.com>
2017-01-14 19:59:36 -0600

    rsync

--- ci/build-artifact.sh
+++ ci/build-artifact.sh
@@ -2,20 +2,6 @@
 
 set -e -x
 
-export TERM=${TERM:-dumb}
-
-apt-get update
-
-apt-get -y install sudo
-
-useradd -m docker && echo "docker:docker" | chpasswd && adduser docker sudo
-
-apt-get -y install openssh-server
-
-apt-get -y install ufw
-
-sudo ufw allow 22
-
 pushd my-product
   ./gradlew clean assemble
 popd
